d9a3221267066624aa4747478c3074426ba5ce96
Karibell <karibel973@gmail.com>
2025-08-18 13:57:08 -0700

    summary

--- ContentAutomator/PostifyNew/socials/vk_groups/vk_get_users/vk_users/search_parameters.py
+++
@@ -1,5 +0,0 @@
-CITY_ID = 2  # change this field to add users from other cities
-FILTER_VALUE = ['']  # filter value (names)
-CITY = 'Москва'  # change this field to add other cities to the db
-YEAR_START = 1965
-YEAR_END = 2006
